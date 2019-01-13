3732093ff791ae939c5608545358e1af1eeca221
marekpraski <praski@student.agh.edu.pl>
2019-01-13 11:16:40 +0100

    dodana metoda newMethod

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,4 +1,8 @@
 public class SomeProgram {
+
+public static void newMethod() {
+//this is some code
+}
     public static void main(String[] args) {
         System.out.println("We are learning to use Git.");
 		System.out.println("We will be Git experts, soon.");
